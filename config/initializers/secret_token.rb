a4d8ee88f64659d9ac778f8f9233ce8b05af937c
App Academy Student <student@appacademy.io>
2013-12-19 10:51:50 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditClone::Application.config.secret_token = '2f8d336dd9a43b9d17d9e1eb23c43211fa3ca67c41def1ff61c4ad761a2b900469ced5db841c6e3cacc89dbfed5d186960fcdc11efcaa2771a6c8359b9a20275'
